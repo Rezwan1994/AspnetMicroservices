9751a1bdecdf95419e5eb1f3bd8cd597c2f88c30
Rezwan1994 <rezwanulhaquerezwan@gmail.com>
2023-09-26 16:21:27 +0600

    Basket Microservice...

---
+++ src/Services/Basket/Basket.API/Reopsitories/IBusketRepository.cs
@@ -0,0 +1,15 @@
+﻿using Basket.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Reopsitories
+{
+    public interface IBusketRepository
+    {
+        Task<ShoppingCart>  GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart userName);
+        Task DeleteBasket(string userName);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GettProducts();
+        Task<IEnumerable<Product>> GettProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string category);
+        Task<Product> GetProduct(string id);
+   
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+
+    }
+}
